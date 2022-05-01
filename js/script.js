b022ca59daf1d6cbc0c1f231588c050fd2d834e9
sfah007 <69042482+sfah007@users.noreply.github.com>
2022-05-01 08:55:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function () {
 
     //little consol msg
     console.log('%c FSSQUAD.com BIN]cc ', "background: #5A8DEE; color: #F2F4F4; font-size: 30px;");
-    console.log('%c Developing_With_Love❤ | 2020 © FSSQUAD.com ', "background: #F2F4F4; color: #5A8DEE; font-size: 10px;");
+    console.log('%c Developing_With_Love❤ | 2022 © wgod.ga ', "background: #F2F4F4; color: #5A8DEE; font-size: 10px;");
 
     // Back to top button
     $(window).scroll(function () {
